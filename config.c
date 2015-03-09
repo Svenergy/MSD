5d2c205ebe4588e69ba8b5e127327bb681a60b83
Svenergy <seo7882@rit.edu>
2015-03-08 23:27:27 -0400

    Updated reading config
    More robust now, will ignore white space and blank lines.  Still some
    structure of the file is required though.

--- config.c
+++ config.c
@@ -7,51 +7,57 @@ void config_fromFile(void){
 	char line[101]; /* Line Buffer */
 	int i = 0;
 	int iVal = 0;
+	char cVal = '';
 	float fVal = 0;
 
 	if (f_open(&config, "config.txt", FA_READ)) {
 		error(ERROR_READ_CONFIG);
 	}
-	/* Read 4 lines to get to update Date/Time */
-	for (i = 0; i < 4; i++) {
-		f_gets(line, sizeof(line), &config);
-	}
+	/* Read lines to get to update Date/Time */
+	getNonBlankLine(line,&config);
+	getNonBlankLine(line,&config);
+	getNonBlankLine(line,&config);
 
 	if (line[0] == 'Y' || line[0] == 'y') {
 		/* Update Date and Time - 4 Lines */
-		f_gets(line, sizeof(line), &config);
-		f_gets(line, sizeof(line), &config);
+		getNonBlankLine(line,&config);
+		getNonBlankLine(line,&config);
 		/* Line is now the date/time string in YYYY-MM-DD HH:MM:SS */
 		setTime(line);
+
+		/* Read lines to get to update config */
+		getNonBlankLine(line,&config);
+		getNonBlankLine(line,&config);
+
+	} else {
+		for (i = 0; i < 4; i++) {
+			getNonBlankLine(line,&config);
+		}
 	}
-	/* Read 4 lines to get to update config */
-	for (i = 0; i < 4; i++) {
-		f_gets(line, sizeof(line), &config);
-	}
+
 	if (line[0] == 'Y' || line[0] == 'y') {
-		/* Update Config - 32 Lines (Maybe) */
-		f_gets(line, sizeof(line), &config);
-		f_gets(line, sizeof(line), &config);
+		/* Update Config - */
+		getNonBlankLine(line,&config);
+		getNonBlankLine(line,&config);
 		/* Line is now the user header */
 		memcpy(daq.user_comment, line, 101);
-		f_gets(line, sizeof(line), &config);
-		f_gets(line, sizeof(line), &config);
-		f_gets(line, sizeof(line), &config);
+		getNonBlankLine(line,&config);
+		getNonBlankLine(line,&config);
 		/* Line is now Output Voltage */
 		sscanf(line, "%f", fVal);
 		daq.mv_out = (int32_t)(fVal * 1000);
-		f_gets(line, sizeof(line), &config);
-		f_gets(line, sizeof(line), &config);
+		getNonBlankLine(line,&config);
+		getNonBlankLine(line,&config);
 		/* Line is now Sample Rate */
 		sscanf(line, "%d", iVal);
 		daq.sample_rate = (int32_t)iVal;
-		f_gets(line, sizeof(line), &config);
-		f_gets(line, sizeof(line), &config);
+		getNonBlankLine(line,&config);
+		getNonBlankLine(line,&config);
 		/* Line is now trigger delay */
 		sscanf(line, "%d", iVal);
 		daq.trigger_delay = iVal;
-		f_gets(line, sizeof(line), &config);
-		f_gets(line, sizeof(line), &config);
+		getNonBlankLine(line,&config);
+		getNonBlankLine(line,&config);
 		/* Line is now data mode */
 		if (line[0] == 'R') {
 			daq.data_type = READABLE;
@@ -59,70 +65,87 @@ void config_fromFile(void){
 			daq.data_type = HEX;
 		} else if (line[0] == 'B') {
 			daq.data_type = BINARY;
+		} else {
+			error(ERROR_READ_CONFIG);
 		}
 		for (i = 0; i<MAX_CHAN; i++) {
-			f_gets(line, sizeof(line), &config);
-			f_gets(line, sizeof(line), &config);
-			f_gets(line, sizeof(line), &config);
+			getNonBlankLine(line,&config);
 
 			/* Channel Config */
 			/* CH Enabled */
-			if (line[23] == 'Y') {
+			strtok(line,":");
+			sscanf(line, "%c", cVal);
+			if (cVal == 'Y') {
 				daq.channel[i].enable = true;
-			} else if (line[23] == 'N') {
+			} else if (cVal == 'N') {
 				daq.channel[i].enable = false;
+			} else {
+				error(ERROR_READ_CONFIG);
 			}
-			f_gets(line, sizeof(line), &config);
+			getNonBlankLine(line,&config);
 			/* CH Range */
-			if (line[23] == '5'){
+			strtok(line,":");
+			sscanf(line, "%c", cVal);
+			if (cVal == '5'){
 				daq.channel[i].range = V5;
-			} else if ((line[23] == '2') && (line[24] == '4')) {
+			} else if (cVal == '2') {
 				daq.channel[i].range = V24;
+			} else {
+				error(ERROR_READ_CONFIG);
 			}
-			f_gets(line, sizeof(line), &config);
+			getNonBlankLine(line,&config);
 
 			/* CH Units */
 			memcpy(daq.channel[i].unit_name, line+23, 9);
-			f_gets(line, sizeof(line), &config);
+			getNonBlankLine(line,&config);
 
 			/* CH Units/Volt */
-			sscanf(line, "UNITS PER VOLT   [fp]: %f", fVal);
+			strtok(line,":");
+			sscanf(line, "%f", fVal);
 			daq.channel[i].units_per_volt = floatToDecFloat(fVal);
-			f_gets(line, sizeof(line), &config);
+			getNonBlankLine(line,&config);
 
 			/* CH Zero Offset */
-			sscanf(line, "ZERO OFFSET      [fp]: %f", fVal);
+			strtok(line,":");
+			sscanf(line, "%f", fVal);
 			daq.channel[i].offset_uV = floatToFix(fVal);
+
+			/* Read lines to get to update calibration */
+			getNonBlankLine(line,&config);
+			getNonBlankLine(line,&config);
+		}
+	} else {
+		for (i = 0; i < 30; i++) {
+			/* Move to next section if no update config */
+			getNonBlankLine(line,&config);
 		}
 	}
-	/* Read 4 lines to get to update calibration */
-	for (i = 0; i < 4; i++) {
-		f_gets(line, sizeof(line), &config);
-	}
-	if (line[0] == 'Y') {
+	if (line[0] == 'Y' || line[0] == 'y') {
 		/* Update Calibration - 18 Lines (Maybe) */
 		for (i = 0; i<MAX_CHAN;i++) {
-			f_gets(line, sizeof(line), &config);
-			f_gets(line, sizeof(line), &config);
-			f_gets(line, sizeof(line), &config);
+			getNonBlankLine(line,&config);
 
 			/* 5V Zero Offset */
-			sscanf(line, "5V ZERO OFFSET  [fp]: %f", fVal);
+			strtok(line,":");
+			sscanf(line, "%f", fVal);
 			daq.channel[i].v5_zero_offset = floatToFix(fVal);
-			f_gets(line, sizeof(line), &config);
+			getNonBlankLine(line,&config);
 
 			/* 5V LSB/Volt */
-			sscanf(line, "5V LSB / VOLT   [fp]: %f", fVal);
+			strtok(line,":");
+			sscanf(line, "%f", fVal);
 			daq.channel[i].v5_uV_per_LSB = floatToFix(fVal);
-			f_gets(line, sizeof(line), &config);
+			getNonBlankLine(line,&config);
 
 			/* 24V Zero Offset */
-			sscanf(line, "24V ZERO OFFSET [fp]: %f", fVal);
+			strtok(line,":");
+			sscanf(line, "%f", fVal);
 			daq.channel[i].v24_zero_offset = floatToFix(fVal);
-			f_gets(line, sizeof(line), &config);
+			getNonBlankLine(line,&config);
 
 			/* 24V LSB/Volt */
-			sscanf(line, "24V LSB / VOLT  [fp]: %f", fVal);
+			strtok(line,":");
+			sscanf(line, "%f", fVal);
 			daq.channel[i].v24_uV_per_LSB = floatToFix(fVal);
 		}
 	}
@@ -160,4 +183,11 @@ char *getTimeStr(){
 	struct tm * timeinfo;
 	timeinfo = localtime(&rawtime);
 	return asctime(timeinfo);
+}
+
+void getNonBlankLine(char* line, FIL* fil){
+	f_gets(line, sizeof(line), &fil);
+	while (line[0] == 0) {
+		f_gets(line, sizeof(line), &fil);
+	}
 }
